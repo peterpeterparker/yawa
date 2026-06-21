70cc540aa215c68f228a086298bf7c36aaea2452
David Dal Busco <david.dalbusco@outlook.com>
2026-06-21 18:23:47 +0200

    feat(tracker): update error msg for renamed init fn (#24)

--- packages/tracker/src/utils/log.utils.ts
+++ packages/tracker/src/utils/log.utils.ts
@@ -1,5 +1,4 @@
-const SERVICES_UNDEFINED_MSG =
-  "Unable to connect to the analytics services. Did you call `initAnalytics`?";
+const SERVICES_UNDEFINED_MSG = "Unable to connect to the analytics services. Did you call `init`?";
 
 export const warningServicesNotInitialized = <T>(value: T) => {
   if (value === undefined || value === null) {
