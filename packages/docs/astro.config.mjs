da7abc8f195f7690f39d4097ba0ecbc304f186cc
David Dal Busco <david.dalbusco@outlook.com>
2026-06-21 18:32:27 +0200

    chore(docs): setup analytics (#20)
    * chore(docs): setup analytics
    
    * feat: use tracker

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -41,6 +41,16 @@ export default defineConfig({
             content: "https://yetanotherwebanalytics.dev/social-image-1.jpg",
           },
         },
+        {
+          tag: "script",
+          attrs: {
+            type: "module",
+            async: true,
+            src: import.meta.env.PROD
+              ? "https://analytics.fluster.io/tracker"
+              : "http://localhost:3000/tracker",
+          },
+        },
       ],
       logo: {
         light: "./src/assets/logo-light.svg",
